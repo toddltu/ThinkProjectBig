9efb3a538d1acf675d06ecea6f87a57d688a3090
TodDltu <toddltu@gmail.com>
2021-05-11 18:57:09 +0300

    Add'ed models, tweek'ed user.php (sanctum token use)

---
+++ database/migrations/2021_05_11_151540_create_post_mens_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Query\Expression;
+
+class CreatePostMensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_mens', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('email')->unique();
+            $table->json('category')->default(new Expression('(JSON_ARRAY())'));
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_mens');
+    }
+}
